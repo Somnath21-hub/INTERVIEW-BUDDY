bbb13195710cda8fa01eb3eb3d6c73642937312e
Somnath Mukherjee <somnath10235@gmail.com>
2026-04-28 15:29:01 +0530

    Refactor index.js and update CORS configuration
    Refactor server/index.js for improved readability and add support for a new frontend origin.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,42 @@
- import express from "express"
-   import dotenv from 'dotenv' 
-import connectDb from "./config/connectDB.js" 
-import cookieParser from "cookie-parser" dotenv.config()
-  import cors from "cors" import authRouter from "./routes/auth.routes.js" 
-import userRouter from "./routes/user.route.js"
-  import interviewRouter from "./routes/interview.route.js"
-    import paymentRouter from "./routes/payment.route.js" 
-const app = express() app.use(cors({ // connecting backend with frontend server origin:"http://localhost:5173", credentials:true })) app.use(express.json()) app.use(express.urlencoded({ extended: true })) app.use(cookieParser()) app.use("/api/auth", authRouter) app.use("/api/user", userRouter) app.use("/api/interview", interviewRouter) app.use("/api/payment",paymentRouter) const PORT=process.env.PORT || 6000 app.listen(PORT,()=>{ console.log(Server running on port ${PORT}) connectDb() })
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/connectDB.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.route.js";
+import interviewRouter from "./routes/interview.route.js";
+import paymentRouter from "./routes/payment.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.set("trust proxy", 1);
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://interview-buddy-1client.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/interview", interviewRouter);
+app.use("/api/payment", paymentRouter);
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDb();
+});
