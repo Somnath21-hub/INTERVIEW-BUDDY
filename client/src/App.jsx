0696b0dbc9815497acdc03fdb5e0f50213d378c4
Somnath21-hub <somnath10235@gmail.com>
2026-04-29 00:18:06 +0530

    Fixed Step2 interview speech and flow logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from './pages/InterviewHistory';
 import InterviewReport from './pages/InterviewReport';
 import Pricing from "./pages/Pricing";
 
-export const ServerUrl = "https://localhost:8000";
+export const ServerUrl = "http://localhost:8000";
 
 function App() {
 
