cde54d904af4d1864cacf919d4f796e120e2e337
Somnath21-hub <somnath10235@gmail.com>
2026-07-11 19:58:07 +0530

    fix: resolve Google Auth dev endpoint and force account select prompt

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -16,4 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app)
 const provider = new GoogleAuthProvider()
+provider.setCustomParameters({
+  prompt: 'select_account'
+})
 export{auth,provider}//to generate popup
